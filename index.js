66b214ebd507d7218e7a39463201f02e17222952
Marwan Bassam <marwanbassam13@gmail.com>
2023-05-26 01:03:25 +0400

    Version 2 initial commit

--- index.js
+++ index.js
@@ -1,183 +1,167 @@
-var back;
-var img;
+// Declare Variables
+var bkgBack;
+var bkgFront;
+var mainScreenElements;
 var btns;
-var compContent;
-var body;
-var outOfZoo;
-var benefitsIsClicked = false;
-var scrollers;
-var scrollContent;
-var currVisibleContent
+var competitionSection;
+var differentSection;
+var tryItSection;
+var qualitySection;
 
-window.onload = function () {
-    back = document.querySelector("#back");
-    img = document.querySelector("#bkg-bottle");
+window.onload = () => {
+    // Initialize Variables
+    bkgBack = document.querySelector("#bkg-back");
+    bkgFront = document.querySelector("#bkg-front");
+    mainScreenElements = document.querySelectorAll(".main-screen");
     btns = document.querySelectorAll(".buttons");
-    compContent = document.querySelector("#comp-content");
-    offerContent = document.querySelector("#offer-content");
-    benefitsContent = document.querySelector("#benefits-content");
-    qualityContent = document.querySelector("#quality-content");
-    body = document.querySelector("body");
-    outOfZooms = document.querySelectorAll(".out-of-zoom");
-    scrollers = document.querySelectorAll(".scroll-content > div")
-    scrollContent = document.querySelector(".scroll-content");
+    competitionSection = document.querySelector("#competition-section");
+    differentSection = document.querySelector("#different-section");
+    tryItSection = document.querySelector("#try-it-section");
+    qualitySection = document.querySelector("#quality-section");
 
-    // -------------------------------------- SCROLL LISTENER ----------------------------------------
-    scrollContent.addEventListener("scroll", (event) => {
-        if (benefitsIsClicked === true) {
-            var yPos = scrollContent.scrollTop;
-
-            if (yPos < 100) {
-                showCurrContent(0);
-            } else if (yPos > 100 && yPos < 200) {
-                showCurrContent(1);
-            } else if (yPos > 200 && yPos < 300) {
-                showCurrContent(2);
-            } else if (yPos > 300) {
-                showCurrContent(3);
-                scrollContent.scrollTo(0, 300);
-            }
-        }
-    })
-}
-
-function showCurrContent(order) {
-    for (let i = 0; i < scrollers.length; i++) {
-        if (i === order) {
-            continue;
-        }
-        const scroller = scrollers[i];
-        scroller.style.animationName = "none";
-        scroller.style.opacity = "0";
-        scroller.style.position = "static";
+    //Background Videos Sync
+    bkgBack.onended = () => {
+        bkgBack.style.display = "none";
+        bkgFront.style.display = "block";
+        bkgFront.play();
     }
 
-    scrollers[order].style.animationName = "appearUp";
-    scrollers[order].style.opacity = "1";
-    scrollers[order].style.position = "fixed";
 }
+
 
 function buttonClick(btnNum) {
     //Hide other elements
-    outOfZooms.forEach(outOfZoom => {
+    mainScreenElements.forEach(outOfZoom => {
         outOfZoom.style.opacity = 0;
     })
 
     switch (btnNum) {
-        case 1:
-            img.style.filter = "blur(5px)";
-            img.style.transformOrigin = "100% 20%";
-            img.style.transform = "scale(1.8)";
-            btns.forEach(btn => {
-                btn.style.transform = "translate(-50vw, 25vh)";
-            });
-            compContent.style.display = "flex";
+        case 1: //Competition Section
+            // Scale Video
+            bkgBack.style.filter = "blur(3px)";
+            bkgBack.style.transformOrigin = "0% 30%";
+            bkgBack.style.transform = "scale(1.8)";
+            bkgFront.style.filter = "blur(3px)";
+            bkgFront.style.transformOrigin = "0% 30%";
+            bkgFront.style.transform = "scale(1.8)";
+            // Hide Buttons
+            btns[1].style.opacity = 0;
+            btns[2].style.opacity = 0;
+            btns[3].style.opacity = 0;
+            // Move Selected Button
+            btns[0].style.transform = "translate(calc(10.5vw + 25%), -10.5vh)";
+            btns[0].style.scale = "2";
+            btns[0].style.backgroundPosition = "left";
+            // Display Section
+            competitionSection.style.display = "block";
             break;
 
-        case 2:
-            img.style.filter = "blur(5px)";
-            img.style.transformOrigin = "right";
-            img.style.transform = "scale(1.8)";
-            btns.forEach(btn => {
-                btn.style.transform = "translate(-50vw, 25vh)";
-            });
-            offerContent.style.display = "flex";
+        case 2: //Different Section
+            // Scale Video
+            bkgBack.style.filter = "blur(3px)";
+            bkgBack.style.transformOrigin = "100% 50%";
+            bkgBack.style.transform = "scale(1.8)";
+            bkgFront.style.filter = "blur(3px)";
+            bkgFront.style.transformOrigin = "100% 50%";
+            bkgFront.style.transform = "scale(1.8)";
+            // Hide Buttons
+            btns[0].style.opacity = 0;
+            btns[2].style.opacity = 0;
+            btns[3].style.opacity = 0;
+            // Move Selected Button
+            btns[1].style.transform = "translate(calc(-9.5vw - 25%), -15vh)";
+            btns[1].style.scale = "2";
+            btns[1].style.backgroundPosition = "right";
+            // Display Section
+            differentSection.style.display = "block";
             break;
 
-        case 3:
-            img.style.filter = "blur(5px)";
-            img.style.transformOrigin = "bottom right";
-            img.style.transform = "scale(1.9)";
-            btns.forEach(btn => {
-                btn.style.transform = "translate(-50vw, 25vh)";
-            });
-            benefitsContent.style.display = "flex";
-            benefitsIsClicked = true;
-            showCurrContent(0)
+        case 3: //Try It Section
+            // Scale Video
+            bkgBack.style.filter = "blur(3px)";
+            bkgBack.style.transformOrigin = "100% 50%";
+            bkgBack.style.transform = "scale(1.8)";
+            bkgFront.style.filter = "blur(3px)";
+            bkgFront.style.transformOrigin = "100% 50%";
+            bkgFront.style.transform = "scale(1.8)";
+            // Hide Buttons
+            btns[0].style.opacity = 0;
+            btns[1].style.opacity = 0;
+            btns[3].style.opacity = 0;
+            // Move Selected Button
+            btns[2].style.transform = "translate(calc(10vw + 25%), -25vh)";
+            btns[2].style.scale = "2";
+            btns[2].style.backgroundPosition = "left";
+            // Display Section
+            tryItSection.style.display = "block";
             break;
 
-        case 4:
-            img.style.filter = "blur(5px)";
-            img.style.transformOrigin = "bottom right";
-            img.style.transform = "scale(1.9)";
-            btns.forEach(btn => {
-                btn.style.transform = "translate(-50vw, 25vh)";
-            });
-            qualityContent.style.display = "flex";
+        case 4: //Quality Section
+            // Scale Video
+            bkgBack.style.filter = "blur(3px)";
+            bkgBack.style.transformOrigin = "100% 50%";
+            bkgBack.style.transform = "scale(1.8)";
+            bkgFront.style.filter = "blur(3px)";
+            bkgFront.style.transformOrigin = "100% 50%";
+            bkgFront.style.transform = "scale(1.8)";
+            // Hide Buttons
+            btns[0].style.opacity = 0;
+            btns[1].style.opacity = 0;
+            btns[2].style.opacity = 0;
+            // Move Selected Button
+            btns[3].style.transform = "translate(calc(-10vw - 25%), -30vh)";
+            btns[3].style.scale = "2";
+            btns[3].style.backgroundPosition = "right";
+            // Display Section
+            qualitySection.style.display = "block";
             break;
 
         default:
             break;
     }
-    // Hide buttons
-    btns[0].style.opacity = 0;
-    btns[1].style.opacity = 0;
-    btns[2].style.opacity = 0;
-    btns[3].style.opacity = 0;
+
     // Back button appear
     back.style.display = "block";
 }
 
-function goBack() {
-    img.style.filter = "blur(0px)";
-    benefitsIsClicked = false;
-
-    // Hide other elements
-    outOfZooms.forEach(outOfZoom => {
-        outOfZoom.style.opacity = 1;
-    })
-
-    img.style.transform = "scale(1)";
-    img.style.zIndex = "0";
-    btns.forEach(btn => {
-        btn.style.transform = "translate(0vw, 0vh)";
-    })
-    compContent.style.display = "none";
-    offerContent.style.display = "none";
-    benefitsContent.style.display = "none";
-    qualityContent.style.display = "none";
-
-    // Show buttons
-    btns[0].style.opacity = 1;
-    btns[1].style.opacity = 1;
-    btns[2].style.opacity = 1;
-    btns[3].style.opacity = 1;
-    // Hide Back button
-    back.style.display = "none";
+// FUNCTION NEEDED FOR SETTIMEOUT IN goBack Function
+function showElement(mainScreenElement) {
+    mainScreenElement.style.opacity = 1;
 }
 
-function order(num, direction) {
-    switch (num) {
-        case 0:
-            if (direction === "back") {
-                //disable
-            } else if (direction === "next") {
-                showCurrContent(1)
-            }
-            break;
-        case 1:
-            if (direction === "back") {
-                showCurrContent(0)
-            } else if (direction === "next") {
-                showCurrContent(2)
-            }
-            break;
-        case 2:
-            if (direction === "back") {
-                showCurrContent(1)
-            } else if (direction === "next") {
-                showCurrContent(3)
-            }
-            break;
-        case 3:
-            if (direction === "back") {
-                showCurrContent(2)
-            } else if (direction === "next") {
-                //disable
-            }
-            break;
+function goBack() {
+    // Show other elements
+    mainScreenElements.forEach(mainScreenElement => {
+        if (mainScreenElement.tagName == 'svg') {
+            setTimeout(showElement, 800, mainScreenElement);
+        } else {
+            mainScreenElement.style.opacity = 1;
+        }
+    })
 
-        default:
-            break;
-    }
+    // Zoom out background video
+    bkgBack.style.filter = "blur(0px)";
+    bkgBack.style.transform = "scale(1)";
+    bkgBack.style.zIndex = "0";
+    bkgFront.style.filter = "blur(0px)";
+    bkgFront.style.transform = "scale(1)";
+    bkgFront.style.zIndex = "0";
+    // Hide hidden sections
+    competitionSection.style.display = "none";
+    differentSection.style.display = "none";
+    tryItSection.style.display = "none";
+    qualitySection.style.display = "none";
+    // return all buttons
+    btns[0].style.backgroundPosition = "right";
+    btns[1].style.backgroundPosition = "left";
+    btns[2].style.backgroundPosition = "right";
+    btns[3].style.backgroundPosition = "left";
+    btns.forEach(btn => {
+        btn.style.transform = "translate(0%, -50%)";
+        btn.style.scale = "1";
+        setTimeout(showElement, 1000, btn)
+    })
+
+    // Hide Back button
+    back.style.display = "none";
 }
